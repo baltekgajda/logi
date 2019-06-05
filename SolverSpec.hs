bd9557af4f3d2a8953bcf72d69d16cb86ff9391e
Dominik Trusinski <dominik.trusinski@gmail.com>
2019-06-05 21:34:47 +0200

    Test to flagFilledFileds

--- SolverSpec.hs
+++ SolverSpec.hs
@@ -31,12 +31,38 @@ main = hspec $ do
                 expectedOutput = [(expectedBoardLine, hints)]
                 actualOutput = toAdvBoardSlices input
             actualOutput `shouldBe` expectedOutput
+    
+    describe "flagFilledFields" $ do
+        it "returns unmodified AdvBoardSlice and False if fields ambiguously placed" $ do
+            let boardLine = [(Blank, False), (Blank, False), (Red, False), (Blank, False), (Blank, False)]
+                hints = [(1, Red, False), (1, Red, False)]
+                input = (boardLine, hints)
+                expectedBoardLine = [(Blank, False), (Blank, False), (Red, False), (Blank, False), (Blank, False)]
+                expectedHints  = [(1, Red, False), (1, Red, False)]
+                expectedBoolVal = False
+                expectedOutput = ((expectedBoardLine, expectedHints), expectedBoolVal)
+                actualOutput = flagFilledFields input
+            actualOutput `shouldBe` expectedOutput
 
--- toAdvBoardSlicesTest1 :: IO ()
--- toAdvBoardSlicesTest1 = print advBoardSlices
---     where 
---         boardSlices = [
---                 ([Blank], [(1, Red, False)]),
---                 ([Blank], [(1, Blue, False)])
---             ]
---         advBoardSlices = toAdvBoardSlices boardSlices
+        it "returns modified AdvBoardSlice and True if fields unambiguously placed - case 1" $ do
+            let boardLine = [(Red, False), (Blank, False), (Red, False)]
+                hints = [(1, Red, False), (1, Red, False)]
+                input = (boardLine, hints)
+                expectedBoardLine = [(Red, True), (NoColor, True), (Red, True)]
+                expectedHints  = [(1, Red, True), (1, Red, True)]
+                expectedBoolVal = True
+                expectedOutput = ((expectedBoardLine, expectedHints), expectedBoolVal)
+                actualOutput = flagFilledFields input
+            actualOutput `shouldBe` expectedOutput
+
+
+        it "returns modified AdvBoardSlice and True if fields unambiguously placed - case 2" $ do
+            let boardLine = [(Red, False), (Black, False), (Blank, False)]
+                hints = [(1, Red, False), (1, Black, False)]
+                input = (boardLine, hints)
+                expectedBoardLine = [(Red, True), (Black, True), (NoColor, True)] -- TODO: Is it proper to mark last field with NoColor ?
+                expectedHints  = [(1, Red, True), (1, Black, True)]
+                expectedBoolVal = True
+                expectedOutput = ((expectedBoardLine, expectedHints), expectedBoolVal)
+                actualOutput = flagFilledFields input
+            actualOutput `shouldBe` expectedOutput
